1d8d4e4581d51ac31d89b6b35adf34c84717f4a4
chefwind <pancakeswap.wind@gmail.com>
2020-11-09 15:06:48 +0000

    feat: add poolinfo to pending api

--- api/pending.ts
+++ api/pending.ts
@@ -1,7 +1,13 @@
 import { NowRequest, NowResponse } from '@now/node';
 import Web3 from 'web3'
+import BigNumber from 'bignumber.js'
 
 const chefABI = require('../contracts/chef')
+
+const getBalanceNumber = (balance: any, decimals = 18) => {
+  const displayBalance = balance.dividedBy(new BigNumber(10).pow(decimals))
+  return displayBalance.toNumber()
+}
 
 const web3 = new Web3(
     new Web3.providers.HttpProvider(
@@ -10,13 +16,18 @@ const web3 = new Web3(
 );
 
 const pending = async (pid: number, address: string) => {
-  const chef = new web3.eth.Contract(chefABI, '0x73feaa1eE314F8c655E354234017bE2193C9E24E');
-  const pending = await chef.methods.pendingCake(pid, address).call()
-  return pending;
+    const chef = new web3.eth.Contract(chefABI, '0x73feaa1eE314F8c655E354234017bE2193C9E24E');
+    const pending = await chef.methods.pendingCake(pid, address).call()
+    const poolInfo = await chef.methods.poolInfo(pid).call()
+    return {
+        pending: getBalanceNumber(new BigNumber(pending)),
+        poolInfo
+    }
+
 }
 
 export default async (_req: NowRequest, res: NowResponse) => {
-  const { address = '0x0F9399FC81DaC77908A2Dde54Bb87Ee2D17a3373', pid='0' } = _req.query
+  const { address = '0x0F9399FC81DaC77908A2Dde54Bb87Ee2D17a3373', pid='1' } = _req.query
   const data = await pending(pid, address)
   res.status(200).send(data);
 };
