9dd406821cb956e3c7606f18496535de3f2c7d09
Philip Kirchner <CrVnsh@users.noreply.github.com>
2020-11-13 08:27:21 +0100

    implement paging and add tests (#2)

--- api/lottery.ts
+++ api/lottery.ts
@@ -1,46 +1,175 @@
-import { NowRequest, NowResponse } from '@now/node';
-import Web3 from 'web3'
+import { NowRequest, NowResponse } from "@now/node";
+import Web3 from "web3";
 
-const lotteryABI = require('../contracts/lottery')
+const lotteryABI = require("../contracts/lottery");
 
 const web3 = new Web3(
-  new Web3.providers.HttpProvider(
-    "https://bsc-dataseed.binance.org"
-  )
+  new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org")
 );
 
-const lottery = async (history:number) => {
-  const lottery = new web3.eth.Contract(lotteryABI, '0x3C3f2049cc17C136a604bE23cF7E42745edf3b91');
+const lotteryContract = new web3.eth.Contract(
+  lotteryABI,
+  "0x3C3f2049cc17C136a604bE23cF7E42745edf3b91"
+);
 
-  const issueIndex = await lottery.methods.issueIndex().call()
-
-  let finalNumbers = []
-  for (let i = issueIndex - 1; i >= 0 && issueIndex - i < history; i--) {
-    let numbers1 = []
-    let numbers2 = []
-    numbers1.push(await lottery.methods.historyNumbers(i, 0).call())
-    numbers1.push(await lottery.methods.historyNumbers(i, 1).call())
-    numbers1.push(await lottery.methods.historyNumbers(i, 2).call())
-    numbers1.push(await lottery.methods.historyNumbers(i, 3).call())
-
-    numbers2.push(await lottery.methods.historyAmount(i, 0).call())
-    numbers2.push(await lottery.methods.historyAmount(i, 1).call())
-    numbers2.push(await lottery.methods.historyAmount(i, 2).call())
-    numbers2.push(await lottery.methods.historyAmount(i, 3).call())
-
-    numbers2 = numbers2.map(n => parseInt(n)/1e18)
-
-    finalNumbers.push({
-      issueIndex: i,
-      numbers1,
-      numbers2
-    })
-  }
-  return finalNumbers
+interface SingleLotteryReturn {
+  numbers1: Promise<[string, string, string, string]>;
+  numbers2: Promise<[string, string, string, string]>;
+  index: number;
+}
+interface Lottery {
+  numbers1: [string, string, string, string];
+  numbers2: Array<number>;
+  issueIndex: number;
 }
 
+/**
+ * Request all Lottery Methods to get the Lottery Data
+ * This Method is not async and is not waiting
+ * This will improve the performance because all requests can be created at almost the same time
+ * @param index
+ */
+const getSingleLottery = (index: number): SingleLotteryReturn => {
+  const numbers1 = Promise.all([
+    lotteryContract.methods.historyNumbers(index, 0).call(),
+    lotteryContract.methods.historyNumbers(index, 1).call(),
+    lotteryContract.methods.historyNumbers(index, 2).call(),
+    lotteryContract.methods.historyNumbers(index, 3).call(),
+  ]);
+  const numbers2 = Promise.all([
+    lotteryContract.methods.historyAmount(index, 0).call(),
+    lotteryContract.methods.historyAmount(index, 1).call(),
+    lotteryContract.methods.historyAmount(index, 2).call(),
+    lotteryContract.methods.historyAmount(index, 3).call(),
+  ]);
+
+  return { numbers1, numbers2, index };
+};
+/**
+ * This function will create the LotteryItem and will resolve the promises
+ * At the time the promises are awaited the request should already be done
+ * @param numbers1Prom Promise with all numbers1 numbers
+ * @param numbers2Prom Promise with all numbers2 numbers
+ * @param index
+ * @param finalNumbers
+ */
+const createLotteryItem = async (
+  numbers1Prom: Promise<[string, string, string, string]>,
+  numbers2Prom: Promise<[string, string, string, string]>,
+  index: number,
+  finalNumbers: Array<Lottery>
+) => {
+  const numbers1 = await numbers1Prom;
+  const numbers2Res = await numbers2Prom;
+  const numbers2: Array<number> = numbers2Res.map((n) => parseInt(n) / 1e18);
+
+  finalNumbers.push({
+    issueIndex: index,
+    numbers1,
+    numbers2,
+  });
+};
+
+/**
+ * It happens that cloudfront rejects request.
+ * To prevent missing lottery Items this retry function requests the lotteryItem again.
+ * @param index
+ * @param finalNumbers
+ * @param retries number of retries
+ */
+const retry = async (
+  index: number,
+  finalNumbers: Array<Lottery>,
+  retries: number
+) => {
+  let retrySuccess = false;
+  let retryCount = 0;
+  while (!retrySuccess && retryCount !== retries) {
+    retryCount++;
+    try {
+      const {
+        numbers1: numbers1Prom,
+        numbers2: numbers2Prom,
+      } = getSingleLottery(index);
+      await createLotteryItem(numbers1Prom, numbers2Prom, index, finalNumbers);
+      retrySuccess = true;
+    } catch (err) {}
+  }
+};
+
+export const lottery = async (
+  pageSize?: number,
+  page: number = 0
+): Promise<{
+  totalPage: number;
+  totalItems: number;
+  lotteries?: Array<Lottery>;
+  currentPage?: number;
+  error?: string;
+  errorMessage?: string;
+}> => {
+  const issueIndex: number = Number(
+    await lotteryContract.methods.issueIndex().call()
+  );
+  const finalNumbers: Array<Lottery> = [];
+  const finalNumbersProm = [];
+  const totalPage = pageSize ? Math.ceil(issueIndex / pageSize - 1) : 0;
+  if (typeof pageSize !== "undefined") {
+    if (pageSize * page > issueIndex) {
+      return {
+        error: "page out of range",
+        errorMessage: `The requested page with the requested pageSize is out of range. The last page is: ${totalPage}`,
+        totalPage,
+        totalItems: issueIndex,
+      };
+    }
+
+    const offset = page * pageSize;
+    const start = issueIndex - (offset + 1);
+    const end = start - pageSize;
+
+    for (let i = start; i >= 0 && i > end; i--) {
+      finalNumbersProm.push(getSingleLottery(i));
+    }
+  } else {
+    for (let i = issueIndex - 1; i >= 0; i--) {
+      finalNumbersProm.push(getSingleLottery(i));
+    }
+  }
+  try {
+    for (let i = 0; i < finalNumbersProm.length; i++) {
+      const {
+        numbers1: numbers1Prom,
+        numbers2: numbers2Prom,
+        index,
+      } = finalNumbersProm[i];
+      try {
+        await createLotteryItem(
+          numbers1Prom,
+          numbers2Prom,
+          index,
+          finalNumbers
+        );
+      } catch (error) {
+        await retry(index, finalNumbers, 3);
+      }
+    }
+  } catch (error) {}
+
+  return {
+    totalPage: totalPage,
+    totalItems: issueIndex - 1,
+    lotteries: finalNumbers,
+    currentPage: page,
+  };
+};
+
 export default async (_req: NowRequest, res: NowResponse) => {
-  const { history = 10 } = _req.query
-  const data = await lottery(history)
+  const { pageSize, page } = _req.query;
+
+  const data = await lottery(
+    typeof pageSize !== "undefined" ? Number(pageSize) : undefined,
+    typeof page !== "undefined" ? Number(page) : undefined
+  );
   res.status(200).send(data);
 };
---
+++ test/lottery.spec.ts
@@ -0,0 +1,49 @@
+import Web3 from "web3";
+import * as lottery from "../api/lottery";
+const lotteryABI = require("../contracts/lottery");
+
+describe("Lottery", () => {
+  let maxLotteries: number;
+  beforeAll(async () => {
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org")
+    );
+
+    const lotteryContract = new web3.eth.Contract(
+      lotteryABI,
+      "0x3C3f2049cc17C136a604bE23cF7E42745edf3b91"
+    );
+
+    maxLotteries = Number(await lotteryContract.methods.issueIndex().call());
+  });
+  it("lottery pagesize 10", async () => {
+    const lotteryResponse = await lottery.lottery(10);
+    expect(lotteryResponse.lotteries).toHaveLength(10);
+  });
+  it("lottery pagesize 20", async () => {
+    const lotteryResponse = await lottery.lottery(20);
+    expect(lotteryResponse.lotteries).toHaveLength(20);
+  });
+  it("lottery no pagesize defined", async () => {
+    const lotteryResponse = await lottery.lottery();
+    expect(lotteryResponse.lotteries).toBeDefined();
+    expect(lotteryResponse.lotteries?.length).toBe(maxLotteries);
+  });
+
+  it("lottery page default", async () => {
+    const lotteryResponse = await lottery.lottery();
+    expect(lotteryResponse.lotteries).toBeDefined();
+    expect(lotteryResponse.lotteries?.length).toBe(maxLotteries);
+  });
+
+  it("lottery page 3 pagesize 10", async () => {
+    const lotteryResponse = await lottery.lottery(10, 3);
+    expect(lotteryResponse.lotteries).toHaveLength(10);
+  });
+
+  it("lottery page 200 pagesize 10", async () => {
+    const lotteryResponse = await lottery.lottery(10, 200);
+    expect(lotteryResponse.error).toBeDefined();
+    expect(lotteryResponse.errorMessage).toBeDefined();
+  });
+});
