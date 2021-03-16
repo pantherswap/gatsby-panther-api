47c329568411d1a1e00013b8dfd1687c55637dfa
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-03-16 12:19:09 +0100

    feat(voting): Historical user balances (#29)
    * feat(voting): Historical user balances
    
    * chore(voting): Use promises
    
    * fix(voting): MasterChef userInfo
    
    * chore(voting): Get poolLength at block

--- utils/web3.ts
+++ utils/web3.ts
@@ -6,14 +6,18 @@ const BSC_NODE_RPC = [
   "https://bsc-dataseed1.ninicoin.io/",
 ];
 
-export const getWeb3 = (): Web3 => {
-  const provider: string = BSC_NODE_RPC[Math.floor(Math.random() * BSC_NODE_RPC.length)];
+const BSC_ARCHIVE_NODE_RPC = ["https://bsc-private-dataseed2.nariox.org/"];
+
+export const getWeb3 = (archive = false): Web3 => {
+  const provider: string = archive
+    ? BSC_ARCHIVE_NODE_RPC[Math.floor(Math.random() * BSC_ARCHIVE_NODE_RPC.length)]
+    : BSC_NODE_RPC[Math.floor(Math.random() * BSC_NODE_RPC.length)];
 
   return new Web3(new Web3.providers.HttpProvider(provider, { timeout: 30000 }));
 };
 
-export const getContract = (abi: any, address: string) => {
-  const web3: Web3 = getWeb3();
+export const getContract = (abi: any, address: string, archive = false) => {
+  const web3: Web3 = getWeb3(archive);
 
   return new web3.eth.Contract(abi, address);
 };
