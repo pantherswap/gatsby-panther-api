7974ce378df59d6ff7110f18b952b362da8d5b33
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-03-19 16:04:54 +0100

    fix(voting): Balance behaviour (#34)

--- utils/balance.ts
+++ utils/balance.ts
@@ -1,9 +1,11 @@
 import BigNumber from "bignumber.js";
 import bep20ABI from "./abis/bep20.json";
+import pairABI from "./abis/pair.json";
 import masterChefABI from "./abis/masterchef.json";
 import smartChefABI from "./abis/smartchef.json";
 import { getContract } from "./web3";
-import { CAKE, MASTERCHEF_CONTRACT } from "./constants";
+import { CAKE, CAKE_BNB_FARM, CAKE_BNB_TOKEN, CAKE_TOKEN, MASTERCHEF_CONTRACT, WBNB_TOKEN } from "./constants";
+import { Pair, TokenAmount } from "@pancakeswap-libs/sdk";
 
 interface UserInfoResult {
   amount: number;
@@ -83,14 +85,43 @@ export const getTotalStaked = async (address: string, block: string): Promise<nu
     const cakeContract = getContract(bep20ABI, CAKE, true);
     const cakeBalance = await cakeContract.methods.balanceOf(address).call(undefined, blockNumber);
     balance = balance.plus(cakeBalance);
+  } catch (error) {
+    console.error(`CAKE balance error: ${error}`);
+  }
 
+  try {
+    // CAKE-BNB farm.
+    const cakeBnbContract = getContract(pairABI, CAKE_BNB_FARM, true);
+    const totalSupplyLP = await cakeBnbContract.methods.totalSupply().call(undefined, blockNumber);
+    const reservesLP = await cakeBnbContract.methods.getReserves().call(undefined, blockNumber);
+    const cakeBnbBalance = await cakeBnbContract.methods.balanceOf(address).call(undefined, blockNumber);
+    const pair: Pair = new Pair(
+      new TokenAmount(CAKE_TOKEN, reservesLP._reserve0.toString()),
+      new TokenAmount(WBNB_TOKEN, reservesLP._reserve1.toString())
+    );
+    const cakeLPBalance = pair.getLiquidityValue(
+      pair.token0,
+      new TokenAmount(CAKE_BNB_TOKEN, totalSupplyLP.toString()),
+      new TokenAmount(CAKE_BNB_TOKEN, cakeBnbBalance.toString()),
+      false
+    );
+    balance = balance.plus(new BigNumber(cakeLPBalance.toSignificant(18)).times(1e18));
+  } catch (error) {
+    console.error(`CAKE-BNB LP error: ${error}`);
+  }
+
+  try {
     // MasterChef contract.
     const masterContract = getContract(masterChefABI, MASTERCHEF_CONTRACT, true);
     const cakeMainStaking: UserInfoResult = await masterContract.methods
       .userInfo(0, address)
       .call(undefined, blockNumber);
     balance = balance.plus(cakeMainStaking.amount);
+  } catch (error) {
+    console.error(`MasterChef error: ${error}`);
+  }
 
+  try {
     // Pools balances.
     const promises = pools.map((pool) => {
       const contract = getContract(smartChefABI, pool, true);
@@ -105,7 +136,9 @@ export const getTotalStaked = async (address: string, block: string): Promise<nu
     );
 
     return balance.plus(balancesMapping).div(1e18).toNumber();
-  } catch (e) {
-    return balance.toNumber();
+  } catch (error) {
+    console.error(`Pools error: ${error}`);
   }
+
+  return balance.toNumber();
 };
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,17 @@
+import { ChainId, Token } from "@pancakeswap-libs/sdk";
+
 // BEP-20 addresses.
 export const CAKE = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82";
+export const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 export const DEAD = "0x000000000000000000000000000000000000dEaD";
 
 // Contract addresses.
+export const CAKE_BNB_FARM = "0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6";
 export const MASTERCHEF_CONTRACT = "0x73feaa1eE314F8c655E354234017bE2193C9E24E";
 export const LOTTERY_CONTRACT = "0x3C3f2049cc17C136a604bE23cF7E42745edf3b91";
 export const MULTICALL_CONTRACT = "0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb";
+
+// PancakeSwap SDK Token.
+export const CAKE_TOKEN = new Token(ChainId.MAINNET, CAKE, 18);
+export const WBNB_TOKEN = new Token(ChainId.MAINNET, WBNB, 18);
+export const CAKE_BNB_TOKEN = new Token(ChainId.MAINNET, CAKE_BNB_FARM, 18);
