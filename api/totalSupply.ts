e75fb3e114708c935165cae9696578a95419e2f7
panther-swap <admin@pantherswap.com>
2021-05-02 00:15:28 +0800

    refactor: Update totalSupply endPoint

--- api/totalSupply.ts
+++ api/totalSupply.ts
@@ -4,5 +4,6 @@ import { getTotalSupply } from "../utils/supply";
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
   let totalSupply = await getTotalSupply();
   totalSupply = totalSupply.div(1e18);
+  res.setHeader('content-type', 'text/plain');
   res.send(totalSupply);
 };
