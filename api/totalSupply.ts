f184f3dd50412e92df3ee84f04ca77227fd993d8
panther-swap <admin@pantherswap.com>
2021-05-02 00:07:22 +0800

    refactor: Add totalSupply endPoint

---
+++ api/totalSupply.ts
@@ -0,0 +1,8 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import { getTotalSupply } from "../utils/supply";
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  let totalSupply = await getTotalSupply();
+  totalSupply = totalSupply.div(1e18);
+  res.send(totalSupply);
+};
